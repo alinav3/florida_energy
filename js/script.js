7c355fafdd1852338ce3c7664079e3d4631c9c3e
alinav3 <alinkin.vk@gmail.com>
2018-05-29 11:18:32 -0500

    add Vue.js

---
+++ js/script.js
@@ -0,0 +1,40 @@
+new Vue({
+  el: "#app",
+  // the instance state
+  data: function() {
+    return {
+      name: "Mike Doe",
+      email: {
+        value: "Mike@gmail.com",
+        valid: true
+      },
+      submitted: false
+    };
+  },
+  methods: {
+    // submit form handler
+    submit: function() {
+      this.submitted = true;
+    },
+    // validate by type and value
+    validate: function(type, value) {
+      if (type === "email") {
+        this.email.valid = this.isEmail(value) ? true : false;
+      }
+    },
+    // check for valid email adress
+    isEmail: function(value) {
+      return emailRegExp.test(value);
+    },
+    // check or uncheck all
+    checkAll: function(event) {
+      this.selection.features = event.target.checked ? this.features : [];
+    }
+  },
+  watch: {
+    // watching nested property
+    "email.value": function(value) {
+      this.validate("email", value);
+    }
+  }
+});
